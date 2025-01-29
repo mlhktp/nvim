0368a0015ac542b2f82c4f95ab23b28c857645dc
Melih Aktop <aktopmelih@gmail.com>
2025-01-30 01:39:32 +0300

    fix: Change copilot completion keymap
    feat: Add other verilog related files

--- lua/night/plugins/lsp/lspconfig.lua
+++ lua/night/plugins/lsp/lspconfig.lua
@@ -1,111 +1,127 @@
 return {
-    "neovim/nvim-lspconfig",
-    event = { "BufReadPre", "BufNewFile" },
-    dependencies = {
-        "hrsh7th/cmp-nvim-lsp",
-        { "antosha417/nvim-lsp-file-operations", config = true },
-        { "folke/neodev.nvim", opts = {} },
-    },
-    config = function()
-        -- import lspconfig plugin
-        local lspconfig = require("lspconfig")
+	"neovim/nvim-lspconfig",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"hrsh7th/nvim-cmp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"saadparwaiz1/cmp_luasnip",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-nvim-lua",
+		"neovim/nvim-lspconfig",
+		"nvim-treesitter/nvim-treesitter",
+		"jose-elias-alvarez/null-ls.nvim",
+		"hrsh7th/cmp-nvim-lsp",
+		{ "antosha417/nvim-lsp-file-operations", config = true },
+		{ "folke/neodev.nvim", opts = {} },
+	},
+	config = function()
+		-- import lspconfig plugin
+		local lspconfig = require("lspconfig")
 
-        -- import mason_lspconfig plugin
-        local mason_lspconfig = require("mason-lspconfig")
+		-- import mason_lspconfig plugin
+		local mason_lspconfig = require("mason-lspconfig")
 
-        -- import cmp-nvim-lsp plugin
-        local cmp_nvim_lsp = require("cmp_nvim_lsp")
+		-- import cmp-nvim-lsp plugin
+		local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-        local keymap = vim.keymap -- for conciseness
+		local keymap = vim.keymap -- for conciseness
 
-        vim.api.nvim_create_autocmd("LspAttach", {
-            group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-            callback = function(ev) end,
-        })
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+			callback = function(ev) end,
+		})
 
-        -- used to enable autocompletion (assign to every lsp server config)
-        local capabilities = cmp_nvim_lsp.default_capabilities()
+		-- used to enable autocompletion (assign to every lsp server config)
+		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-        -- Change the Diagnostic symbols in the sign column (gutter)
-        local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-        for type, icon in pairs(signs) do
-            local hl = "DiagnosticSign" .. type
-            vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-        end
+		-- Change the Diagnostic symbols in the sign column (gutter)
+		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+		for type, icon in pairs(signs) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+		end
 
-        mason_lspconfig.setup_handlers({
-            -- default handler for installed servers
-            function(server_name)
-                lspconfig[server_name].setup({
-                    capabilities = capabilities,
-                })
-            end,
-            ["svelte"] = function()
-                -- configure svelte server
-                lspconfig["svelte"].setup({
-                    capabilities = capabilities,
-                    on_attach = function(client, bufnr)
-                        vim.api.nvim_create_autocmd("BufWritePost", {
-                            pattern = { "*.js", "*.ts" },
-                            callback = function(ctx)
-                                -- Here use ctx.match instead of ctx.file
-                                client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
-                            end,
-                        })
-                    end,
-                })
-            end,
+		mason_lspconfig.setup_handlers({
+			-- default handler for installed servers
+			function(server_name)
+				lspconfig[server_name].setup({
+					capabilities = capabilities,
+				})
+			end,
+			["svelte"] = function()
+				-- configure svelte server
+				lspconfig["svelte"].setup({
+					capabilities = capabilities,
+					on_attach = function(client, bufnr)
+						vim.api.nvim_create_autocmd("BufWritePost", {
+							pattern = { "*.js", "*.ts" },
+							callback = function(ctx)
+								-- Here use ctx.match instead of ctx.file
+								client.notify("$/onDidChangeTsOrJsFile", { uri = ctx.match })
+							end,
+						})
+					end,
+				})
+			end,
 
-            ["graphql"] = function()
-                -- configure graphql language server
-                lspconfig["graphql"].setup({
-                    capabilities = capabilities,
-                    filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
-                })
-            end,
-            ["emmet_ls"] = function()
-                -- configure emmet language server
-                lspconfig["emmet_ls"].setup({
-                    capabilities = capabilities,
-                    filetypes = {
-                        "html",
-                        "typescriptreact",
-                        "javascriptreact",
-                        "css",
-                        "sass",
-                        "scss",
-                        "less",
-                        "svelte",
-                    },
-                })
-            end,
-            ["lua_ls"] = function()
-                -- configure lua server (with special settings)
-                lspconfig["lua_ls"].setup({
-                    capabilities = capabilities,
-                    settings = {
-                        Lua = {
-                            -- make the language server recognize "vim" global
-                            diagnostics = {
-                                globals = { "vim" },
-                            },
-                            completion = {
-                                callSnippet = "Replace",
-                            },
-                        },
-                    },
-                })
-            end,
-            ["matlab_ls"] = function()
-                lspconfig["matlab_ls"].setup({
-                    filetypes = { "matlab" },
-                    settings = {
-                        matlab = {
-                            installPath = "/usr/local/MATLAB/R2024b/",
-                        },
-                    },
-                })
-            end,
-        })
-    end,
+			["graphql"] = function()
+				-- configure graphql language server
+				lspconfig["graphql"].setup({
+					capabilities = capabilities,
+					filetypes = { "graphql", "gql", "svelte", "typescriptreact", "javascriptreact" },
+				})
+			end,
+			["emmet_ls"] = function()
+				-- configure emmet language server
+				lspconfig["emmet_ls"].setup({
+					capabilities = capabilities,
+					filetypes = {
+						"html",
+						"typescriptreact",
+						"javascriptreact",
+						"css",
+						"sass",
+						"scss",
+						"less",
+						"svelte",
+					},
+				})
+			end,
+			["lua_ls"] = function()
+				-- configure lua server (with special settings)
+				lspconfig["lua_ls"].setup({
+					capabilities = capabilities,
+					settings = {
+						Lua = {
+							-- make the language server recognize "vim" global
+							diagnostics = {
+								globals = { "vim" },
+							},
+							completion = {
+								callSnippet = "Replace",
+							},
+						},
+					},
+				})
+			end,
+			["matlab_ls"] = function()
+				lspconfig["matlab_ls"].setup({
+					filetypes = { "matlab" },
+					settings = {
+						matlab = {
+							installPath = "/usr/local/MATLAB/R2024b/",
+						},
+					},
+				})
+			end,
+			["svlangserver"] = function()
+				lspconfig["svlangserver"].setup({
+					capabilities = capabilities,
+					filetypes = { "systemverilog", "verilog" },
+				})
+			end,
+		})
+	end,
 }
--- lua/night/plugins/nvim-cmp.lua
+++ lua/night/plugins/nvim-cmp.lua
@@ -1,98 +1,119 @@
 return {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-        "neovim/nvim-lspconfig",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-        "onsails/lspkind.nvim",
-    },
-    config = function()
-        -- Set up nvim-cmp.
-        local cmp = require("cmp")
+	"hrsh7th/nvim-cmp",
+	dependencies = {
+		"neovim/nvim-lspconfig",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"hrsh7th/nvim-cmp",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"onsails/lspkind.nvim",
+	},
+	config = function()
+		-- Set up nvim-cmp.
+		local cmp = require("cmp")
 
-        cmp.setup({
-            formatting = {
-                format = require("lspkind").cmp_format({
-                    mode = "symbol",
-                    maxwidth = 50,
-                    ellipsis_char = "...",
-                    symbol_map = { Copilot = "" },
-                }),
-                fields = { "kind", "abbr", "menu" },
-                max_width = 0,
-                source_names = {
-                    nvim_lsp = "(LSP)",
-                    emoji = "(Emoji)",
-                    path = "(Path)",
-                    calc = "(Calc)",
-                    cmp_tabnine = "(Tabnine)",
-                    vsnip = "(Snippet)",
-                    luasnip = "(Snippet)",
-                    buffer = "(Buffer)",
-                    tmux = "(TMUX)",
-                    copilot = "(Copilot)",
-                    treesitter = "(TreeSitter)",
-                },
-            },
-            window = {
-                completion = cmp.config.window.bordered(),
-            },
-            mapping = cmp.mapping.preset.insert({
-                ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-                ["<C-f>"] = cmp.mapping.scroll_docs(4),
-                ["<C-Space>"] = cmp.mapping.complete(),
-                ["<C-e>"] = cmp.mapping.abort(),
-                ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-            }),
-            sources = cmp.config.sources({
-                { name = "nvim_lsp" },
-                { name = "copilot" },
-                { name = "vsnip" }, -- For vsnip users.
-                -- { name = 'luasnip' }, -- For luasnip users.
-                { name = "ultisnips" }, -- For ultisnips users.
-                { name = "snippy" }, -- For snippy users.
-            }, {
-                { name = "buffer" },
-            }),
-        })
+		cmp.setup({
+			formatting = {
+				format = require("lspkind").cmp_format({
+					mode = "symbol",
+					maxwidth = 50,
+					ellipsis_char = "...",
+					symbol_map = { Copilot = "" },
+				}),
+				fields = { "kind", "abbr", "menu" },
+				max_width = 0,
+				source_names = {
+					nvim_lsp = "(LSP)",
+					emoji = "(Emoji)",
+					path = "(Path)",
+					calc = "(Calc)",
+					cmp_tabnine = "(Tabnine)",
+					vsnip = "(Snippet)",
+					luasnip = "(Snippet)",
+					buffer = "(Buffer)",
+					tmux = "(TMUX)",
+					copilot = "(Copilot)",
+					treesitter = "(TreeSitter)",
+				},
+			},
+			window = {
+				completion = cmp.config.window.bordered(),
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-p>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.abort(),
+				["<C-l>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+				["<A-l>"] = function(fallback)
+					cmp.mapping.abort()
+					local copilot_keys = vim.fn["copilot#Accept"]()
+					if copilot_keys ~= "" then
+						vim.api.nvim_feedkeys(copilot_keys, "i", true)
+					else
+						fallback()
+					end
+				end,
+			}),
 
-        -- Set configuration for specific filetype.
-        cmp.setup.filetype("gitcommit", {
-            sources = cmp.config.sources({
-                { name = "git" }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
-            }, {
-                { name = "buffer" },
-            }),
-        })
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "copilot" },
+				{ name = "vsnip" }, -- For vsnip users.
+				-- { name = 'luasnip' }, -- For luasnip users.
+				{ name = "ultisnips" }, -- For ultisnips users.
+				{ name = "snippy" }, -- For snippy users.
+			}, {
+				{ name = "buffer" },
+			}),
+		})
 
-        -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-        cmp.setup.cmdline({ "/", "?" }, {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = {
-                { name = "buffer" },
-            },
-        })
+		-- Set configuration for specific filetype.
+		cmp.setup.filetype("gitcommit", {
+			sources = cmp.config.sources({
+				{ name = "git" }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+			}, {
+				{ name = "buffer" },
+			}),
+		})
 
-        -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-        cmp.setup.cmdline(":", {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = cmp.config.sources({
-                { name = "path" },
-            }, {
-                { name = "cmdline" },
-            }),
-        })
+		-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+		cmp.setup.cmdline({ "/", "?" }, {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = {
+				{ name = "buffer" },
+			},
+		})
 
-        -- Set up lspconfig.
-        local capabilities = require("cmp_nvim_lsp").default_capabilities()
-        -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-        --require("lspconfig")["<YOUR_LSP_SERVER>"].setup({
-        --  capabilities = capabilities,
-        --})
-    end,
+		-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+		cmp.setup.cmdline(":", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = cmp.config.sources({
+				{ name = "path" },
+			}, {
+				{ name = "cmdline" },
+			}),
+		})
+
+		-- cmp.mapping["<C-y>"] = function(fallback)
+		--    cmp.mapping.abort()
+		--    local copilot_keys = vim.fn["copilot#Accept"]()
+		--    if copilot_keys ~= "" then
+		--       vim.api.nvim_feedkeys(copilot_keys, "i", true)
+		--    else
+		--       fallback()
+		--    end
+		-- end
+		--
+
+		-- Set up lspconfig.
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+		--require("lspconfig")["<YOUR_LSP_SERVER>"].setup({
+		--  capabilities = capabilities,
+		--})
+	end,
 }
