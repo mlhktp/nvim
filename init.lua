11da3a2e229ddb5d87495397daa2e47b0cd0e567
Melih Aktop <aktopmelih@gmail.com>
2025-02-13 01:27:40 +0300

    feat: Add smoother transitions

--- init.lua
+++ init.lua
@@ -39,6 +39,11 @@ nlspsettings.setup({})
 if vim.g.neovide then
     -- Put anything you want to happen only in Neovide here
    --
-   vim.o.guifont = "Source Code Pro:h9" -- text below applies for VimScript
+   vim.o.guifont = "FiraCode Nerd Font:h8.5:x8.0" -- text below applies for VimScript
+   vim.g.neovide_padding_top = 20
+   vim.g.neovide_padding_bottom = 30
+   vim.g.neovide_padding_right = 20
+   vim.g.neovide_padding_left = 20
+   -- font line size 140%
 end
 
