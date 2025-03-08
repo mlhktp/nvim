c2e8bc83471d662435c3262a96f158aa95bcadf5
Melih Aktop <aktopmelih@gmail.com>
2025-03-08 16:36:18 +0300

    fix: Add hidden files to telescope

--- lua/night/plugins/telescope.lua
+++ lua/night/plugins/telescope.lua
@@ -1,4 +1,5 @@
-return {
+return
+{
     "nvim-telescope/telescope.nvim",
     branch = "0.1.x",
     dependencies = {
@@ -9,6 +10,19 @@ return {
     config = function()
         require("telescope").setup({
             pickers = {
+                find_files = {
+                    hidden = true, -- Show hidden files (like `.config`)
+                    no_ignore = true, -- Include files ignored by `.gitignore`
+                    file_ignore_patterns = {".git/*", "./.git/", "./node_modules/*", "node_modules", "^node_modules/*", "node_modules/*"}
+                },
+                live_grep = {
+                    additional_args = function()
+                        return {
+                            "--hidden", -- Include hidden files
+                            "--glob", "!.git/*" -- Exclude `.git` directory
+                        }
+                    end
+                },
                 colorscheme = {
                     enable_preview = true,
                 },
@@ -16,3 +30,4 @@ return {
         })
     end,
 }
+
