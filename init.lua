7ffa7c51432a4360e3780248bf68259929d80c5c
Melih Aktop <aktopmelih@gmail.com>
2025-04-21 21:41:17 +0300

    fix: Migrate the options from the init.lua to options.lua.

--- init.lua
+++ init.lua
@@ -22,19 +22,4 @@ vim.cmd("colorscheme kanagawa")
 -- solarized-osaka, solarized-osaka-day, solarized-osaka-moon, solarized-osaka-storm, solarized-osaka-night
 
 
-vim.opt.list = true
-vim.opt.listchars:append("space: ")
-
-if vim.g.neovide then
-    -- Put anything you want to happen only in Neovide here
-   --
-   vim.o.guifont = "FiraCode Nerd Font:h8.5:x8.0" -- text below applies for VimScript
-   vim.g.neovide_padding_top = 20
-   vim.g.neovide_padding_bottom = 30
-   vim.g.neovide_padding_right = 20
-   vim.g.neovide_padding_left = 20
-   vim.keymap.set({ "n", "v" }, "<C-+>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
-   vim.keymap.set({ "n", "v" }, "<C-_>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
-   -- font line size 140%
-end
 
