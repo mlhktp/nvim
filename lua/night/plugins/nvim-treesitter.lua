c2d07e5e6bff54afbae91a8da23e3530ddbcbaf6
Melih Aktop <aktopmelih@gmail.com>
2025-04-21 23:28:23 +0300

    fix: Disable highlighting for treesitter verilog.

--- lua/night/plugins/nvim-treesitter.lua
+++ lua/night/plugins/nvim-treesitter.lua
@@ -4,14 +4,13 @@ return {
       event = { "BufReadPre", "BufNewFile" },
       build = ":TSUpdate",
       dependencies = {
-         "nvim-treesitter/nvim-treesitter-textobjects",
          "windwp/nvim-ts-autotag",
          "p00f/nvim-ts-rainbow",
       },
       opts = {
             highlight = {
                 enable = true,
-                disable = {},
+                disable = { "verilog", "systemverilog" }, -- list of languages to disable highlighting for
                 -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
                 --  If you are experiencing weird indenting issues, add the language to
                 --  the list of additional_vim_regex_highlighting and disabled languages for indent.
