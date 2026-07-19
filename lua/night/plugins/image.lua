7c57b6d6f10f504a5586ce29cd6bff601acc761d
Melih Aktop <aktopmelih@gmail.com>
2026-07-19 14:22:04 +0200

    fix: Add macos updates.

---
+++ lua/night/plugins/image.lua
@@ -0,0 +1,9 @@
+return {
+  "3rd/image.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  opts = {
+    backend = "kitty", -- or "ueberzug", "wezterm"
+  },
+}
--- lua/night/plugins/nvim-cmp.lua
+++ lua/night/plugins/nvim-cmp.lua
@@ -44,13 +44,13 @@ return {
 
          mapping = cmp.mapping.preset.insert({
             ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-            ["<C-N>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
-            ["<C-p>"] = cmp.mapping.scroll_docs(-4),
-            ["<C-f>"] = cmp.mapping.scroll_docs(4),
+            ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+            -- ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+            -- ["<C-f>"] = cmp.mapping.scroll_docs(4),
             ["<C-Space>"] = cmp.mapping.complete(),
             ["<C-e>"] = cmp.mapping.abort(),
-            ["<C-l>"] = cmp.mapping.confirm({ select = true }),
-            ["<A-l>"] = function(fallback)
+            ["<C-Enter>"] = cmp.mapping.confirm({ select = true }),
+            ["<C-l>"] = function(fallback)
                cmp.mapping.abort()
                local copilot_keys = vim.fn["copilot#Accept"]()
                if copilot_keys ~= "" then
